70d1e89294666bcbea95d9805878f8a142f8ecac
alex <alex@codeception.de>
2015-06-21 20:13:34 +0200

    added config script

--- scripts/config.lua
+++ scripts/config.lua
@@ -4,7 +4,26 @@ engineConfig = {
     sceneScript = "scene.lua",
     loadScript  = "loadScene.lua",
 
+    -- context 
+    resolutionX = 1280,
+    resolutionY = 720,
+    windowTitle = "IsoEngine",
+
     -- camera
+    fieldOfView  = 75,
+    acceleration = 0.01,
+    turnSpeed    = 0.01,
+    nearClip     = 0.01,
+    farClip      = 100000,
+    aspectRatio  = 16/9,
+    smoothing    = 0.1,
+
+    -- texture
+    anisotropicLvl = 16
+
+
+
+    
 
 
 
